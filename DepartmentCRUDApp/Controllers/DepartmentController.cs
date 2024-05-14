a1c5dd940e2a93feb87595e6c395843bd46f4977
hala hamdan <halahamdan@gmail.com>
2024-05-14 23:54:17 +0300

    first

---
+++ DepartmentCRUDApp/Controllers/DepartmentController.cs
@@ -0,0 +1,38 @@
+﻿using DepartmentCRUDApp.Data;
+using DepartmentCRUDApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DepartmentCRUDApp.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var departments = context.DepartmentSet.ToList();
+            return View("CRUD", departments);
+        }
+        public IActionResult Details(int id)
+        {
+            var department = context.DepartmentSet.Find(id);
+            return View("Details",department);
+        }
+        public IActionResult Create(int id)
+        {
+            return View("Create");
+        }
+        public IActionResult Store(Department department)
+        {
+            context.DepartmentSet.Add(department);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id) {
+            var department = context.DepartmentSet.Find(id);
+            context.DepartmentSet.Remove(department);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DepartmentCRUDApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DepartmentCRUDApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DepartmentCRUDApp.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Department> DepartmentSet { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=CRUD;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
